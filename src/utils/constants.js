34c1fab8af282aa4e4b435564c1b321fda67e8bf
Albert Taygibov <albert.ti.2017@yandex.ru>
2024-07-10 00:09:24 +0300

    fix: readme

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,23 +1,24 @@
+const YP_LEARN_URL = 'https://nomoreparties.co/v1/plus-cohort-21'
+const MY_DB_URL = 'http://api.albert-ti.students.nomoredomainsmonster.ru'
+
 export const config = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-21',
+  baseUrl: MY_DB_URL,
   headers: {
     authorization: '1bdbc94b-6239-4b7b-83a9-133ed323b1e4',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   },
-};
-
+}
 
 export const dataSelectors = {
   formSelector: '.form',
   inputSelector: '.form__place',
   submitButtonSelector: '.form__button',
   inactiveButtonClass: 'form__button_inactive',
-  errorClass: 'form__place_active-error'
+  errorClass: 'form__place_active-error',
 }
-
 
 export const formData = {
   nameSelector: '.profile__name',
   aboutSelector: '.profile__text',
-  avatarSelector: '.profile__avatar-img'
+  avatarSelector: '.profile__avatar-img',
 }
